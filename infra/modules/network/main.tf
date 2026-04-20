cb133d25a6659953b6aa10465c1e9b75383461b4
Prashant Agarwal <prashantag@google.com>
2026-04-20 17:22:13 +0530

    Enable VPC Flow Logs for org policy

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -25,6 +25,12 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.id
   project       = var.project_id
   private_ip_google_access = true
+
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
 
 resource "google_compute_global_address" "private_ip_address" {
